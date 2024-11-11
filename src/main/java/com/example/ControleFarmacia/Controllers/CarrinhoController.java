3e2394bbd4b0e9b81ace3a0d9ef870cd5dd0459a
DANILO ALVES GUIMARAES <daguimaraes@tjsp.jus.br>
2024-11-11 17:20:32 -0300

    começo da construçao da pagina de cadastro

---
+++ src/main/java/com/example/ControleFarmacia/Controllers/CarrinhoController.java
@@ -0,0 +1,17 @@
+package com.example.ControleFarmacia.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/home/carrinho")
+public class CarrinhoController {
+    @GetMapping
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("carrinhoIndex");
+        return mv;
+    }
+}
